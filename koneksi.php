30057034844893f92c4dd3005003e7c56ad867df
urdhanaka <urdhanaka.aptanagi@gmail.com>
2024-01-01 05:16:24 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$host = "localhost"; // Nama hostnya
+$username = "user"; // Username
+$password = ""; // Password (Isi jika menggunakan password)
+$database = "crud-foto"; // Nama databasenya
+// Koneksi ke MySQL dengan PDO
+$pdo = new PDO('mysql:host=' . $host . ';dbname=' . $database, $username, $password);
